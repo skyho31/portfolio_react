c167cb5c17cf4cf2491b0de110b79478bf5aaa3b
shkim <shkim@n3n.co.kr>
2018-08-20 09:59:48 +0900

    bug fix

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,11 +4,10 @@ import '../../styles/contact.scss';
 const Contact = () => (
   <div className="contactContainer">
     <section className="middleBox" style={{fontSize:"16px", width:"100%"}}>
-        <img alt="logo" src={process.env.PUBLIC_URL + "/imgs/dieRever.png"} />
+        <img src="./imgs/dieRever.png" alt="logo"/>
         <p style={{color:"#ff6666", fontSize:"1.5em"}}><b>Thank you for visiting! :)</b></p>
         <p>skyho31@naver.com</p>
         <a href="https://github.com/skyho31" style={{color:'black', fontSize:'1.5em', fontFamily:'Open Sans Condensed, sans-serif'}}>https://github.com/skyho31</a>
-        <p>test1</p>
     </section>
   </div>
 );
